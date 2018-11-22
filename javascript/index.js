91ee21b2eadccd24355d07061ddd511ca711f323
Hal Helms <hal.helms@gmail.com>
2018-11-21 16:53:48 -0800

    21NOV1700

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,4 @@
 function toggle(element) {
-  console.log(element.nextElementSibling);
   let details = element.nextElementSibling;
-  if (details.style.display === "none") {
-    details.style.display = "block";
-  } else {
-    details.style.display = "none"
-  }
+  details.classList.toggle('hidden');
 }
